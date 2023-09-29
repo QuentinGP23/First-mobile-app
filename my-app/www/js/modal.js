6217c2bb9ed29908487d816c1f0d9259bb070d6d
QuentinGP <quentgeoffpro@yahoo.com>
2023-09-29 13:27:04 +0200

    .

--- my-app/www/js/modal.js
+++ my-app/www/js/modal.js
@@ -2,3 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.modal');
   var instances = M.Modal.init(elems);
 });
+
+const addItem = function(){
+  var todoItem = $("#todo-item").val();
+  var itemTemplate =
+  '<li class="collection-item">' +
+  '<div>' + todoItem +
+  '<a class="secondary-content">' +
+  '<label>' +
+  '<input type="checkbox" class="filled-in" onclick="moveItemDone(this);" />' +
+  '<span></span>' +
+  '</label>' +
+  '</a>' +
+  '</div>' +
+  '</li>';
+  $("#my-todo-list").append(itemTemplate);
+  $("#todo-item").val('');
+  }
